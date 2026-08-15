efe3e6fe134c13d2016016d9b0835cab99e76b70
Rizky Saputra Latief <rizkysaputralatief1093@gmail.com>
2026-08-15 12:57:52 +0700

    Use environment variable for API URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "http://localhost:5205/api";
+
 export const api = axios.create({
-  baseURL: "http://localhost:5205/api",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
